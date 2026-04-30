ba3c7373090e958d4462341011f83a6f888f2084
Dinda Azra <azra070506@gmail.com>
2026-04-30 20:25:07 +0700

    Refactor login component (add comments)

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -56,7 +56,8 @@ const submit = () => {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
                         </div>
-                       <!-- Input email user -->
+
+                        <!-- Input email user -->
                         <input
                             id="email"
                             type="email"
@@ -81,6 +82,7 @@ const submit = () => {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
                             </svg>
                         </div>
+                        
                         <!-- Input password dengan toggle visibility -->
                         <input
                             id="password"
@@ -125,6 +127,7 @@ const submit = () => {
                 </div>
 
                 <div class="pt-2">
+                
                 <!-- Tombol login dengan animasi dan loading -->
                     <button
                         type="submit"
